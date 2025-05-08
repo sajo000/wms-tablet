7099a57b4b1932fbdaecafeb7126e6456869c3a5
sajo000 <sajo@deleo.co.kr>
2025-05-08 17:33:19 +0900

    PLFPL-1272 기본 레이아웃 퍼블리싱

--- front/function.js
+++ front/function.js
@@ -1,32 +1,22 @@
 /**
  * SIDEBAR 스크립트
  */
-$(document).ready(function () {
-  const $btnToggle = $('.btn-sidebar-toggle');
-  if ($btnToggle.length === 0) {
-    console.warn('버튼 요소를 찾을 수 없습니다.');
+$(document).on('click', '.btn-sidebar-toggle', function () {
+  const $leftSide = $('.left-side');
+  const $icon = $(this).find('.icon-sidebar-toggle');
+
+  if ($leftSide.length === 0 || $icon.length === 0) {
+    console.warn('필요한 요소를 찾을 수 없습니다.');
     return;
   }
 
-  $btnToggle.on('click', function () {
-    const $leftSide = $('.left-side');
-    const $icon = $(this).find('.icon-sidebar-toggle'); // . 추가
+  $leftSide.toggleClass('collapsed');
 
-    if ($leftSide.length === 0 || $icon.length === 0) {
-      console.warn('필요한 요소를 찾을 수 없습니다.');
-      return;
-    }
-
-    $leftSide.toggleClass('collapsed');
-
-    if ($icon.hasClass('icon-arrow-left')) {
-      $icon.removeClass('icon-arrow-left').addClass('icon-arrow-right');
-    } else {
-      $icon.removeClass('icon-arrow-right').addClass('icon-arrow-left');
-    }
-
-    // 메뉴 텍스트 숨기기/보이기 처리는 CSS에서 transition과 opacity로 처리
-  });
+  if ($icon.hasClass('icon-arrow-left')) {
+    $icon.removeClass('icon-arrow-left').addClass('icon-arrow-right');
+  } else {
+    $icon.removeClass('icon-arrow-right').addClass('icon-arrow-left');
+  }
 });
 
 /**
--- front/layout.js
+++ front/layout.js
@@ -1,17 +1,8 @@
 // 사이드바 연결
-// fetch("pages/layout/sidebar.html")
-// .then((response) => {
-//   return response.text();
-// })
-// .then((data) => {
-//   document.querySelector("#sidebar").innerHTML = data;
-// });
-
-// 풋터 연결
-fetch("pages/layout/footer.html")
+fetch("pages/layout/sidebar.html")
 .then((response) => {
   return response.text();
 })
 .then((data) => {
-  document.querySelector("#footer").innerHTML = data;
+  document.querySelector("#sidebar").innerHTML = data;
 });
