71b4fce907eb2dfcf1dba1adfd4a0c38591c7efc
sajo000 <sajo@deleo.co.kr>
2025-05-08 10:37:59 +0900

    PLFPL-1272 레이아웃 작업중

--- front/layout.js
+++ front/layout.js
@@ -1,26 +1,17 @@
-// 헤더 연결
-fetch("pages/layout/header.html")
+// 사이드바 연결
+// fetch("pages/layout/sidebar.html")
+// .then((response) => {
+//   return response.text();
+// })
+// .then((data) => {
+//   document.querySelector("#sidebar").innerHTML = data;
+// });
+
+// 풋터 연결
+fetch("pages/layout/footer.html")
 .then((response) => {
   return response.text();
 })
 .then((data) => {
-  document.querySelector("#header").innerHTML = data;
+  document.querySelector("#footer").innerHTML = data;
 });
-
-// 풋터 연결
-// fetch("../../pages/layout/footer.html")
-// .then((response) => {
-//   return response.text();
-// })
-// .then((data) => {
-//   document.querySelector("#footer").innerHTML = data;
-// });
-
-// 모바일 풋터 네비 연결
-// fetch("../../pages/layout/footer-nav-mobile.html")
-// .then((response) => {
-//   return response.text();
-// })
-// .then((data) => {
-//   document.querySelector("#footer-nav-mobile").innerHTML = data;
-// });
