8e8a1fc554dbdb48ab55f202ff47204956fe158d
sajo000 <sajo@deleo.co.kr>
2025-05-12 09:49:10 +0900

    PLFPL-1313 레이아웃 퍼블리싱중

--- front/function.js
+++ front/function.js
@@ -12,13 +12,45 @@ $(document).on('click', '.btn-sidebar-toggle', function () {
 
   $leftSide.toggleClass('collapsed');
 
-  if ($icon.hasClass('icon-arrow-left')) {
-    $icon.removeClass('icon-arrow-left').addClass('icon-arrow-right');
+  if ($icon.hasClass('icon-sidebar-arrow-left')) {
+    $icon.removeClass('icon-sidebar-arrow-left').addClass(
+        'icon-sidebar-arrow-right');
   } else {
-    $icon.removeClass('icon-arrow-right').addClass('icon-arrow-left');
+    $icon.removeClass('icon-sidebar-arrow-right').addClass(
+        'icon-sidebar-arrow-left');
   }
 });
 
 /**
  * PAGE 스크립트
  */
+$(function(){
+  // 셀렉트 열기/닫기
+  $(document).on('click', '.custom-select', function(e){
+    $('.custom-select').not(this).removeClass('open');
+    $('.custom-select-list').not($(this).next()).hide();
+    $(this).toggleClass('open');
+    $(this).next('.custom-select-list').toggle();
+    e.stopPropagation();
+  });
+
+  // 옵션 선택
+  $(document).on('click', '.custom-select-list li', function(e){
+    var $wrap = $(this).closest('.custom-select-wrap');
+    var value = $(this).data('value');
+    var text = $(this).text();
+    $wrap.find('.custom-select-selected').text(text);
+    $wrap.find('input[type="hidden"]').val(value);
+    $wrap.find('.custom-select-list li').removeClass('selected');
+    $(this).addClass('selected');
+    $wrap.find('.custom-select').removeClass('open');
+    $wrap.find('.custom-select-list').hide();
+    e.stopPropagation();
+  });
+
+  // 외부 클릭 시 닫기
+  $(document).on('click', function(){
+    $('.custom-select').removeClass('open');
+    $('.custom-select-list').hide();
+  });
+});
